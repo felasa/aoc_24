ffacc17e34d7d86e1112a5b21afe6befa8b81276
Federico Lasa <felasa+github@gmail.com>
2024-06-21 11:26:38 -0600

    Day 6 parts 1&2. Still struggling with 5-2

---
+++ src/aoc_6.clj
@@ -0,0 +1,51 @@
+(ns aoc-6 
+  (:require
+    [clojure.string :as str]))
+
+(def file "input_6")
+
+(defn parse-input 
+  [file]
+  (->> 
+    file 
+    slurp
+    str/split-lines
+    (map #(rest (str/split % #" +")))
+    (map #(map parse-long %))))
+
+(defn distance
+  [hold-time race-time]
+  (* hold-time (- race-time (min hold-time race-time))))
+
+(defn n-ways
+  [race-time record]
+  (let [options (range 1 race-time)]
+    (count (filter #(> % record) (map #(distance % race-time) options)))))
+ 
+(defn solution-1
+  [file]
+  (let [data (parse-input file)
+        [times records] data]
+    (->> 
+      (map vector times records)
+      (map #(apply n-ways %))
+      (reduce *)))) 
+
+(solution-1 file)
+(defn parse-input-2 
+  [file]
+  (as-> 
+    file x 
+    (slurp x)
+    (str/replace x #" +" "")
+    (str/split-lines x)
+    (map #(re-find #"\d+" %) x)
+    (map parse-long x)))
+      
+;slow but w/e
+(defn solution-2 
+  [file]
+  (apply n-ways (parse-input-2 file)))
+ 
+(solution-2 file)
+
