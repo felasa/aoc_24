9eeda5bec4995c2600085efe60803512a40d60a9
felasa <felasa@gmail.com>
2024-12-18 17:28:29 -0600

    Day 5 part 2 lazy

--- src/day_05.clj
+++ src/day_05.clj
@@ -31,11 +31,6 @@
 97,13,75,29,47
 ")
 
-(defn split-with-ex
-  "Split with but exludes the matching line"
-  [pred coll]
-  [(take-while pred coll) (rest (drop-while pred coll))])
-
 (defn parse-rules
   "Takes the rules and returns a key map grouped by 
    the leading number and a collection of following numbers"
@@ -43,7 +38,6 @@
   (let [g (->> ruleset
               (map #(s/split % #"\|"))
               (map (partial map parse-long))
-              ;(map (partial apply hash-map))))
               (group-by first))]
     (update-vals g #(set (map second %)))))
   
@@ -69,7 +63,6 @@
   (loop [checked (list (first updates))
          remaining (rest updates)]
     (if (empty? remaining) true
-      ;if-let?
       (let [head (first remaining)
             tail (rest remaining)]
         (if (rules head)
@@ -94,22 +87,41 @@
          (reduce +))))
 
 (comment 
+  (def example-data (parse-input example-input))
   (solution-1 example-input)   ;; 143
   (solution-1 (slurp "resources/input/input_05"))) ;; 7024
 
 (defn s1 []
   (solution-1 (slurp "resources/input/input_05"))) 
+
 ;; PART 2
 
-(let [data (parse-input example-input)
-      rules (data :rules)
-      update-set (data :updates)]
-  (filter (complement (partial validate-updates rules)) update-set))
+;; Lazy solution using comparators
+(defn ordering 
+  [rules]
+  (fn [l r] (if (and (rules l) ((rules l) r)) true false)))
 
-(defn swap-index
-  "Lazy seq from 0 with numebrs i and j swapped"
-  [i j]
-  (if (< i j)
-    (concat (range 0 i) (list j) (range (inc i) j) (list i) (map #(+ % j 1) (range)))
-    (concat (range 0 j) (list i) (range (inc j) i) (list j) (map #(+ % j 1) (range)))))
+(comment 
+  (let [example-ordering (ordering (:rules example-data))]
+    (sort (comparator example-ordering) [75 97 47 61 53]) ;; (97 75 47 61 53)
+    (sort (comparator example-ordering) [61 13 29]) ;; (61 29 13)
+    (sort (comparator example-ordering) [97 13 75 29 47]))) ;; (97 75 47 29 13)
+
+(defn solution-2 [s]
+  (let [data (parse-input s)
+        rules (data :rules)
+        update-set (data :updates)
+        rules-comparator (ordering rules)]
+    (->> update-set
+         (filter (complement (partial validate-updates rules)))
+         (map (partial sort rules-comparator))
+         (map get-middle)
+         (reduce +))))
+
+(comment 
+  (solution-2 example-input) ;; 123
+  (solution-2 (slurp "resources/input/input_05"))) ;; 4151
+
+(defn s2 []
+  (solution-2 (slurp "resources/input/input_05"))) ;; 4151
 
