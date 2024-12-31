7f876e58fd04977bd94f4c0b339c9abec8369b45
felasa <felasa@gmail.com>
2024-12-31 13:06:30 -0600

    Day 12 part 2

--- src/day_12.clj
+++ src/day_12.clj
@@ -91,7 +91,69 @@ MMMISSJEEE")
   (solution-1 (slurp "resources/input/input_12")))
 
 ;; PART 2
-;;  should be able to count sides from the locations generated prev
-(sort (update-vals (group-by #(% 0) (:locations (first (survey example-data)))) sort))
+;;  We count sides from the set of locations generated previously
+;;  countinh the hirizontal ones first then the vertical ones
+(defn count-hsides
+  "Given a set of locations return the number of horizontal sides"
+  [locations]
+  (let [;locations (:locations (first (survey example-data)))
+        min-row (reduce min (map #(% 0) locations)) 
+        max-row (reduce max (map #(% 0) locations)) 
+        min-col (reduce min (map #(% 1) locations))
+        max-col (reduce max (map #(% 1) locations))]
+    (loop [row min-row
+           col min-col
+           sides 0
+           counting-top? false
+           counting-bottom? false]
+      ;(println {:coord [row col] :sides sides :ct counting-top? :cb counting-bottom?})
+      (if (> row max-row) sides
+        (if (> col max-col)
+          (recur (inc row) min-col sides false false)
+          (if (locations [row col])
+            (let [neighbor-top (locations [(dec row) col])
+                  neighbor-bottom (locations [(inc row) col])
+                  add-top?    (not (or neighbor-top counting-top?))
+                  add-bottom? (not (or neighbor-bottom counting-bottom?))]
+               (recur row (inc col) 
+                      (+ sides (if add-top? 1 0)
+                               (if add-bottom? 1 0))
+                      (if neighbor-top false true)
+                      (if neighbor-bottom false true)))
+            (recur row (inc col) sides false false)))))))
 
+;Just reverse the coords
+(defn count-vsides 
+  "Given a set of locations count the vertical sides"
+  [locations]
+  (count-hsides (into #{} (map #(into [] (reverse %)) locations))))
+
+(defn count-sides 
+  "Returns the total count of sides (+ horizontal vertical)"
+  [locations]
+  (+ (count-hsides locations) (count-vsides locations)))
+
+(defn add-sides
+  "Adds a key of sides to a map containing the locations"
+  [surveyed]
+  (assoc surveyed :sides (count-sides (:locations surveyed))))
+
+(defn bulk-cost 
+  "Computs fencing bulk cost"
+  [surveyed]
+  (* (:area surveyed) (:sides surveyed)))
+
+(defn solution-2
+  [s]
+  (->> s parse-input survey 
+       (map add-sides)
+       (map bulk-cost)
+       (reduce +)))
+
+(comment 
+  (solution-2 example-input) ;; 1206
+  (solution-2 (slurp "resources/input/input_12"))) ;; 784982
+
+(defn s2 []
+  (solution-2 (slurp "resources/input/input_12")))
 
