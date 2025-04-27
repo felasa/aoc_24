2e10b8abd1095095692683625b502525c0ef31b7
felasa <felasa@gmail.com>
2025-04-27 11:03:41 -0600

    2023 day 17 part 2 + corections

--- src/y2023/day_17.clj
+++ src/y2023/day_17.clj
@@ -1,6 +1,5 @@
 (ns y2023.day-17
   (:require [clojure.string :as s]
-            [util.util :refer [queue]]
             [clojure.data.priority-map :refer [priority-map]]))
 
 (def example-input "2413432311323
@@ -27,17 +26,18 @@
 (def directions [[0 1] [1 0] [-1 0] [0 -1]])
 
 (defn move
-  "Moves coordinate in direction given. return new state"
+  "Moves coordinate in direction given. Returns new state"
   [cities dir [coord heat direction consecutive]]
   (let [newpos (mapv + coord dir)
         newheat (+ heat (get-in cities newpos ##Inf))
         newcount (if (= direction dir) (inc consecutive) 1)]
     [newpos newheat dir newcount]))
 
-;; Initially i tried solving this a simple queue that just move one step at a time
+;; Initially I tried solving this a simple queue that just moved one step at a time
 ;;  while enforcing the consecutive rule. This didnt work since we couldn't accurately
-;;  keep track of optimal paths, and when we did it was slooow. All solutions I saw looked ahead 
-;; to populate the possible the queue with better options first.
+;;  keep track of optimal paths and when we did it was slooow. All solutions I saw looked ahead 
+;; to populate the possible the queue with better options first. 
+;; this + priority map finally cracked it.
 ;; References:
 ;; (without priority map!)
 ;; https://github.com/abyala/advent-2023-clojure/blob/main/src/advent_2023_clojure/day17.clj 
@@ -56,6 +56,7 @@
   (all-straight-moves example-data [[5 5] 5 [0 1] 2] 1 3 [1 0]))
 ;; ([[6 5] 12 [1 0] 1] [[7 5] 19 [1 0] 2] [[8 5] 25 [1 0] 3])
 
+;; babby's first a*
 (defn prio-fn
   "Returns a priority for a next move based on distance to end and current cost (heat)"
   [end [npos nheat _ _]]
@@ -65,7 +66,7 @@
 (defn next-moves-weighted
   "next moves with priority
    1. Moving towards end. 
-   2. Prefer consecutive moves. 3. ??heat???"
+   2. less accumulated heat"
   [cities [cpos cheat cdir ccons]]
   (let [end [(dec (count (first cities))) (dec (count cities))]]
     (->> directions
@@ -112,4 +113,44 @@
 (comment 
   (solution-1 example-input) ;; 102
   (solution-1 (slurp "resources/input/2023/input_17"))) ;; 694
+;; PART 2
+;; we generalize some funs
+
+(defn gnext-moves-weighted
+  "next moves with priority
+   1. Moving towards end. 
+   2. less accumulated heat"
+  [minsteps maxsteps cities [cpos cheat cdir ccons]]
+  (let [end [(dec (count (first cities))) (dec (count cities))]]
+    (->> directions
+         (remove #(= (mapv + cdir %) [0 0]))
+         ;(map (move-fn cities [cpos cheat cdir ccons]))
+         (mapcat (partial all-straight-moves cities [cpos cheat cdir ccons] minsteps maxsteps))
+         ;(remove nil?)
+         (map #(vector % ((partial prio-fn end) %))))))
+
+(defn gtravel
+  [minsteps maxsteps cities]
+  (let [H (dec (count cities)) W (dec (count (first cities)))]
+    (loop [Q1 (priority-map [[0 0] 0 nil 0] 0)
+           visited {}]
+      (let [[[node vheat dir _ :as visiting] _] (peek Q1)]
+        (if (= node [H W])
+          vheat
+          (if (get visited [node dir])
+            (recur (pop Q1) visited)
+            (let [nns ((partial gnext-moves-weighted minsteps maxsteps) cities visiting)]
+              (recur (into (pop Q1) nns)
+                     (assoc visited [node dir] vheat)))))))))
+
+(comment
+  (gtravel 1 3 example-data) ;; 102
+  (gtravel 1 3 (parse-input (slurp "resources/input/2023/input_17"))))  ;; 694
+
+(defn solution-2 [s]
+  (->> s parse-input (gtravel 4 10)))
+
+(comment
+  (solution-2 example-input) ;; 94
+  (solution-2 (slurp "resources/input/2023/input_17"))) ;; 829
 
